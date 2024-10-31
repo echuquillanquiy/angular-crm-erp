d87d49066133260fc195346d9e9480da58890ded
echuquillanquiy <edward.axl3@gmail.com>
2024-10-31 16:33:53 -0500

    ANGULAR, COMIENZO

---
+++ src/app/modules/roles/roles-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {RolesComponent} from "./roles.component";
+import * as path from "node:path";
+import {ListRolesComponent} from "./list-roles/list-roles.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RolesComponent,
+    children: [
+      {
+        path: 'list',
+        component: ListRolesComponent
+      },
+
+      {
+        path: 'list',
+        component: ListRolesComponent
+      },
+
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RolesRoutingModule { }
